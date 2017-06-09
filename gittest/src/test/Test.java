796a57654ff667f9bf4588e68faeba603d344adc
yn <yn@yn-PC>
2017-06-09 11:45:06 +0800

    show(s)

--- gittest/src/test/Test.java
+++ gittest/src/test/Test.java
@@ -4,8 +4,8 @@ public class Test {
 	public int sum(int x,int y) {
 		return x+y;		
 	}
-	private void show() {
-		
+	private void show(String s) {
+		System.out.println(s);
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
